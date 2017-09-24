0abb1150e56b218a081fd7a1dab47d314ecdbdb5
Sergio del Rio <oftheriver@gmail.com>
2017-09-24 11:58:27 +0200

    Add build-timestamp plugin. Fix JenkinsMasterExecutors on init script.

--- executors.groovy
+++ executors.groovy
@@ -1,6 +1,7 @@
 import jenkins.model.*
 
 def jenkinsMasterExecutors = System.getenv("JENKINS_MASTER_EXECUTORS") ?: "4"
+jenkinsMasterExecutors = jenkinsMasterExecutors.substring(1, jenkinsMasterExecutors.length()-1)
 Jenkins.instance.setNumExecutors(jenkinsMasterExecutors.toInteger())
 print "Jenkins Master Executors to " + jenkinsMasterExecutors + "\n"
 
